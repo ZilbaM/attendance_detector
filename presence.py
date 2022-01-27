846e3a5e0ee416345fa90e6b93e0bef5e0ed0e91
Arthur <arthur.lucas@edu.devinci.fr>
2022-01-27 10:16:06 +0100

    can pass robot.txt & create an SSL key

--- presence.py
+++ presence.py
@@ -7,35 +7,15 @@ import re
 import mechanize
 from mechanize import Browser
 
-
-import os
-result = os.popen("curl https://www.leonard-de-vinci.net/robots.txt").read()
-result_data_set = {"Disallowed":[], "Allowed":[]}
-
-for line in result.split("\n"):
-    if line.startswith('Allow'):    # this is for allowed url
-        result_data_set["Allowed"].append(line.split(': ')[1].split(' ')[0])    # to neglect the comments or other junk info
-    elif line.startswith('Disallow'):    # this is for disallowed url
-        result_data_set["Disallowed"].append(line.split(': ')[1].split(' ')[0])    # to neglect the comments or other junk info
-
-print (result_data_set)
+# Ignore robots.txt.  Do not do this without thought and consideration.
 
 
->>> urllib.robotparser.RobotFileParser()
->>> urllib.robotparser.RobotFileParser().set_url("https://www.leonard-de-vinci.net/robots.txt")
->>> urllib.robotparser.RobotFileParser().read()
->>> rrate = urllib.robotparser.RobotFileParser().request_rate("*")
->>> rrate.requests
+br = mechanize.Browser()
+br.set_handle_robots(False)
 
->>> rrate.seconds
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
 
->>> urllib.robotparser.RobotFileParser().crawl_delay("*")
+br.open("https://www.leonard-de-vinci.net/student/presences/")
 
->>> urllib.robotparser.RobotFileParser().can_fetch("*", "https://www.leonard-de-vinci.net/student/presences/")
-
-
-"""br = mechanize.Browser()
-br.open("https://www.leonard-de-vinci.net/")
-
-print(br.title())
-soup = BeautifulSoup(html_doc, 'html.parser') """
+print(br.geturl())
